50392fe41edc199bb1daa2a321d0bc9791795eaa
mrtboy <mohammadreza_t2001@yahoo.com>
2017-12-24 00:36:38 +0100

    Setup dev dependencues and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public/bundles'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -44,8 +44,9 @@ module.exports = (env) => {
     ],
     devtool: isProdcution ? 'source-map' : 'inline-source-map',
     devServer: {
-      contentBase: path.join(__dirname, 'public/bundles'),
-      historyApiFallback: true
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   };
 
