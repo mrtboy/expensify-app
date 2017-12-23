218d4081800a6605f6315dabb92808152f03ea94
mrtboy <mohammadreza_t2001@yahoo.com>
2017-12-23 23:13:28 +0100

    Setup production webpack build

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import getVisisbleExpenses from '../selectors/expenses';
+
 const ExpenseList = (props) => (
   <div>
-    <h1>Expense list</h1>
-    {props.expenses.map((expense) =>
-      <ExpenseListItem key={expense.id} {...expense} />)}
+    {
+      props.expenses.length === 0 ? (
+        <p>No Expenses</p>
+      ) : (
+        props.expenses.map((expense) =>
+      <ExpenseListItem key={expense.id} {...expense} />)
+      )
+    }
+    
   </div>
 );
 
