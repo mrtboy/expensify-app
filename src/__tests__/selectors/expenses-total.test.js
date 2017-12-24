f56731619318cc4ccf0d91c637321b2f13785954
mrtboy <mohammadreza_t2001@yahoo.com>
2017-12-24 14:55:54 +0100

    Add summary expenses

---
+++ src/__tests__/selectors/expenses-total.test.js
@@ -0,0 +1,17 @@
+import selectExpensesTotal from '../../selectors/expenses-total';
+import expenses from '../../testData/fixtures/expenses';
+
+test("return 0 if no expenses", () => {
+  const total = selectExpensesTotal([]);
+  expect(total).toBe(0);
+});
+
+test("add up a single expense", () => {
+  const result = selectExpensesTotal([expenses[0]]);
+  expect(result).toBe(195);
+});
+
+test("add up multiple expenses", () => {
+  const result = selectExpensesTotal(expenses);
+  expect(result).toBe(114195);
+});
